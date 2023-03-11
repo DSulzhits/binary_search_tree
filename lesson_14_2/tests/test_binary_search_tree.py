dd2e73e41116738c8bc454c52cd6d9c29a2bc2c4
DSulzhits <dsulzhits@gmail.com>
2023-03-11 15:56:09 +0300

    Add test for BST for 100% coverage.

--- lesson_14_2/tests/test_binary_search_tree.py
+++ lesson_14_2/tests/test_binary_search_tree.py
@@ -1,6 +1,7 @@
 import pytest
 from lesson_14_2.src.binary_search_tree import BinarySearchTree
 
+
 @pytest.fixture
 def bst():
     bst = BinarySearchTree()
@@ -18,12 +19,28 @@ def test_bst_insert_root(bst):
     """При добавлении единственного узла он хранится в root"""
     assert bst.root.data == {'id': 40}
 
+
 def test_bst_insert_left(bst):
     """Добавляем первый элемент слева"""
     bst.insert({'id': 30})
     assert bst.root.left.data == {'id': 30}
 
+
 def test_bst_insert_right(bst):
-    """Добавляем первый элемент слева"""
+    """Добавляем первый элемент справа"""
     bst.insert({'id': 50})
     assert bst.root.right.data == {'id': 50}
+
+
+def test_bst_insert_left_left(bst):
+    """Добавляем два элемента слева"""
+    bst.insert({'id': 30})
+    bst.insert({'id': 25})
+    assert bst.root.left.left.data == {'id': 25}
+
+
+def test_bst_insert_right_right(bst):
+    """Добавляем два элемента справа"""
+    bst.insert({'id': 50})
+    bst.insert({'id': 60})
+    assert bst.root.right.right.data == {'id': 60}
